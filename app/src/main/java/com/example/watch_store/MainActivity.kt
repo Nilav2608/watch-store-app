7737ded146b65874c03198cced9e0df0972d1580
Nilavarasu2608 <knlsu444@gmail.com>
2022-08-03 11:58:49 +0530

    1.0

--- app/src/main/java/com/example/watch_store/MainActivity.kt
+++ app/src/main/java/com/example/watch_store/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.watch_store
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+
     }
 }
